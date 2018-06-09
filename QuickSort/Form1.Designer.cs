e75c28bab10d2f03dc3980e1fc63ab2cd2f49f1a
Yuri Brandao <claudio.y07@gmail.com>
2018-06-08 21:33:34 -0300

    Projeto

---
+++ QuickSort/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace QuickSort
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblLista = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblListaOrdenada = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblQtd = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(69, 53);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(132, 20);
+            this.txtNum.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(230, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Adcionar na lista";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnAdicionar);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Numero";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Lista";
+            // 
+            // lblLista
+            // 
+            this.lblLista.AutoSize = true;
+            this.lblLista.Location = new System.Drawing.Point(72, 125);
+            this.lblLista.Name = "lblLista";
+            this.lblLista.Size = new System.Drawing.Size(0, 13);
+            this.lblLista.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(69, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Listar Ordenada";
+            // 
+            // lblListaOrdenada
+            // 
+            this.lblListaOrdenada.AutoSize = true;
+            this.lblListaOrdenada.Location = new System.Drawing.Point(72, 175);
+            this.lblListaOrdenada.Name = "lblListaOrdenada";
+            this.lblListaOrdenada.Size = new System.Drawing.Size(0, 13);
+            this.lblListaOrdenada.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(230, 153);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Ordenar Lista";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btnOrdenar);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(230, 108);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(114, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Limpar Lista";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.btnLimpar);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(119, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "(max.: 9)";
+            // 
+            // lblQtd
+            // 
+            this.lblQtd.AutoSize = true;
+            this.lblQtd.Location = new System.Drawing.Point(105, 108);
+            this.lblQtd.Name = "lblQtd";
+            this.lblQtd.Size = new System.Drawing.Size(0, 13);
+            this.lblQtd.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(376, 236);
+            this.Controls.Add(this.lblQtd);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblListaOrdenada);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblLista);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtNum);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Projeto QuickSort";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblLista;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblListaOrdenada;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblQtd;
+    }
+}
+
---
+++ QuickSort/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuickSort
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int[] numeros = new int[9];
+        int cont = 0;
+
+        private void btnAdicionar(object sender, EventArgs e)
+        {
+            int num = Convert.ToInt32(txtNum.Text);
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                if (numeros[i] == 0)
+                {
+                    numeros[i] = num;
+                    lblLista.Text = lblLista.Text + "-" + numeros[i];
+                    cont++;
+                    lblQtd.Text ="" + cont;
+
+                    break;
+                }
+            }
+        }
+
+        public void QuickSort(int[] vetor, int inicio, int fim)
+        {
+            numeros = vetor;
+            int baixo, alto, meio, pivo, repositorio;
+            baixo = inicio;
+            alto = fim;
+            meio = (int)((baixo + alto) / 2);
+
+            pivo = vetor[meio];
+
+            while (baixo <= alto)
+            {
+                while (vetor[baixo] < pivo)
+                    baixo++;
+                while (vetor[alto] > pivo)
+                    alto--;
+                if (baixo < alto)
+                {
+                    repositorio = vetor[baixo];
+                    vetor[baixo++] = vetor[alto];
+                    vetor[alto--] = repositorio;
+                }
+                else
+                {
+                    if (baixo == alto)
+                    {
+                        baixo++;
+                        alto--;
+                    }
+                }
+            }
+
+            if (alto > inicio)
+                QuickSort(vetor, inicio, alto);
+            if (baixo < fim)
+                QuickSort(vetor, baixo, fim);
+        }
+
+        private void btnOrdenar(object sender, EventArgs e)
+        {
+            QuickSort(numeros, 0, numeros.Length - 1);
+
+            lblListaOrdenada.Text = "";
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                lblListaOrdenada.Text = lblListaOrdenada.Text + "-" + numeros[i];
+            }
+        }
+
+        private void btnLimpar(object sender, EventArgs e)
+        {
+            lblLista.Text = "";
+            cont = 0;
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                if (numeros[i] != 0)
+                {
+                    numeros[i] = 0;
+                }
+            }
+        }
+    }
+}
